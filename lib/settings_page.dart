5585145a2807d4fa065f993deaa40269503d2159
Capitaozila <estudanteluis3030@gmail.com>
2023-06-27 12:24:24 -0300

    removendo a opção de selecionar quantidade de img

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -24,79 +24,20 @@ class _SettingsPageState extends State<SettingsPage> {
       appBar: AppBar(
         title: const Text('Configurações'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
+            Text(
               'Criadores:',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 8),
-            const Text('Fulano'),
-            const Text('Ciclano'),
-            const SizedBox(height: 16),
-            const Text(
-              'Quantidade de Imagens:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: _imageCountController,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                      labelText: 'Número de Imagens (1-20)',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Digite um número válido';
-                      }
-                      final imageCount = int.tryParse(value);
-                      if (imageCount == null ||
-                          imageCount < 1 ||
-                          imageCount > 20) {
-                        return 'Digite um número válido (1-20)';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                const SizedBox(width: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    final imageCount = int.tryParse(_imageCountController.text);
-                    if (imageCount != null &&
-                        imageCount >= 1 &&
-                        imageCount <= 20) {
-                      Navigator.pop(context, imageCount);
-                    } else {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: const Text('Erro'),
-                            content: const Text('Digite um número válido (1-20)'),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('OK'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    }
-                  },
-                  child: const Text('Salvar'),
-                ),
-              ],
-            ),
+            SizedBox(height: 8),
+            Text('Fulano'),
+            Text('Ciclano'),
+            SizedBox(height: 16),
+            SizedBox(height: 8),
           ],
         ),
       ),
